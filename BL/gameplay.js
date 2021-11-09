e2d41ef4cd413e8effb767fa85e98bc660af202d
Piyush <piyush2396@gmail.com>
2021-11-09 16:44:10 +0530

    code cleanup

--- BL/gameplay.js
+++ BL/gameplay.js
@@ -7,69 +7,100 @@ import isEmpty from '../utils/isEmpty.js';
 
 export const onPlayerMove = async ({ gameID, roomID, rowID, columnID }) => {
     try {
+        let message = 'no winner yet';
+        let currentGame = { move: [] };
+        let nextMove = '';
+        let winnerID = undefined;
         if (rowID >= 0 && rowID <= 2 && columnID >= 0 && columnID <= 2) {
-            const game = await findGameByID(gameID);
+            currentGame = await findGameByID(gameID);
 
-            let currentGame = game;
             if (!isEmpty(currentGame.moves.length)) {
                 const exists = currentGame.moves.find(x => x.rowID === rowID && x.columnID === columnID);
                 if (exists) {
                     return { message: 'Illegal Move', gameData: generateGameData(currentGame), nextMove: currentGame.nextMoveID };
-                }
-                const currentMove = currentGame.moves[currentGame.moves.length - 1].type === 'cross' ? 'circle' : 'cross';
+                } else {
+                    const currentMove = currentGame.moves[currentGame.moves.length - 1].type === 'cross' ? 'circle' : 'cross';
 
+                    currentGame = await addMove(gameID, {
+                        move: {
+                            roomID,
+                            type: currentMove,
+                            rowID,
+                            columnID,
+                            createdAt: new Date()
+                        },
+                        nextMove: currentMove === 'cross' ? 'circle' : 'cross',
+                        nextMoveID: currentGame.nextMoveID === currentGame.roomIDs.creator ? currentGame.roomIDs.joinee : currentGame.roomIDs.creator
+                    });
+
+                    // Check if current player has won the game
+                    const sameRowData = currentGame.moves.filter(x => x.rowID === rowID);
+                    const completeRowStrike = sameRowData.length === 3 && sameRowData.every(x => x.type === currentMove);
+                    if (completeRowStrike) {
+                        message = 'winner';
+                        currentGame = generateGameData(currentGame);
+                        nextMove = currentGame.nextMoveID;
+                        winnerID = roomID;
+                    } else {
+                        const sameColumnData = currentGame.moves.filter(x => x.columnID === columnID);
+                        const completeColumnStrike = sameColumnData.length === 3 && sameColumnData.every(x => x.type === currentMove);
+                        if (completeColumnStrike) {
+                            message = 'winner';
+                            nextMove = currentGame.nextMoveID;
+                            currentGame = generateGameData(currentGame);
+                            winnerID = roomID;
+                        } else if ((rowID + columnID) % 2 === 0) {
+                            const firstDiagonalData = currentGame.moves.filter((x, i) => (x.columnID === 0 && x.rowID === 2) || (x.columnID === 1 && x.rowID === 1) || (x.columnID === 2 && x.rowID === 0));
+                            const completeFirstDiagonalStrike = firstDiagonalData.length === 3 && firstDiagonalData.every(x => x.type === currentMove);
+                            if (completeFirstDiagonalStrike) {
+                                message = 'winner';
+                                currentGame = generateGameData(currentGame);
+                                nextMove = currentGame.nextMoveID;
+                                winnerID = roomID;
+                            } else {
+                                const secondDiagonalData = currentGame.moves.filter((x) => x.columnID === x.rowID);
+                                const completeSecondDiagonalStrike = secondDiagonalData.length === 3 && secondDiagonalData.every(x => x.type === currentMove);
+                                if (completeSecondDiagonalStrike) {
+                                    message = 'winner';
+                                    currentGame = generateGameData(currentGame);
+                                    nextMove = currentGame.nextMoveID;
+                                    winnerID = roomID;
+                                } else {
+                                    message = 'no winner yet';
+                                    nextMove = currentGame.nextMoveID;
+                                    currentGame = generateGameData(currentGame);
+                                }
+                            }
+                        } else {
+                            message = 'no winner yet';
+                            nextMove = currentGame.nextMoveID;
+                            currentGame = generateGameData(currentGame);
+                        }
+                    }
+
+                }
+            } else {
                 currentGame = await addMove(gameID, {
                     move: {
                         roomID,
-                        type: currentMove,
+                        type: 'cross',
                         rowID,
                         columnID,
                         createdAt: new Date()
                     },
-                    nextMove: currentMove === 'cross' ? 'circle' : 'cross',
-                    nextMoveID: currentGame.nextMoveID === currentGame.roomIDs.creator ? currentGame.roomIDs.joinee : currentGame.roomIDs.creator
+                    nextMove: 'circle',
+                    nextMoveID: currentGame.roomIDs.joinee
                 });
-
-                // Check if current player has won the game
-                const sameRowData = currentGame.moves.filter(x => x.rowID === rowID);
-                const completeRowStrike = sameRowData.length === 3 && sameRowData.every(x => x.type === currentMove);
-                if (completeRowStrike) {
-                    return { message: 'winner', gameData: generateGameData(currentGame), nextMove: currentGame.nextMoveID, winnerID: roomID };
-                }
-
-                const sameColumnData = currentGame.moves.filter(x => x.columnID === columnID);
-                const completeColumnStrike = sameColumnData.length === 3 && sameColumnData.every(x => x.type === currentMove);
-                if (completeColumnStrike) {
-                    return { message: 'winner', gameData: generateGameData(currentGame), nextMove: currentGame.nextMoveID, winnerID: roomID };
-                }
-
-                if ((rowID + columnID) % 2 === 0) {
-                    const firstDiagonalData = currentGame.moves.filter((x, i) => (x.columnID === 0 && x.rowID === 2) || (x.columnID === 1 && x.rowID === 1) || (x.columnID === 2 && x.rowID === 0));
-                    const completeFirstDiagonalStrike = firstDiagonalData.length === 3 && firstDiagonalData.every(x => x.type === currentMove);
-                    if (completeFirstDiagonalStrike) {
-                        return { message: 'winner', gameData: generateGameData(currentGame), nextMove: currentGame.nextMoveID, winnerID: roomID };
-                    }
-                    const secondDiagonalData = currentGame.moves.filter((x) => x.columnID === x.rowID);
-                    const completeSecondDiagonalStrike = secondDiagonalData.length === 3 && secondDiagonalData.every(x => x.type === currentMove);
-                    if (completeSecondDiagonalStrike) {
-                        return { message: 'winner', gameData: generateGameData(currentGame), nextMove: currentGame.nextMoveID, winnerID: roomID };
-                    }
-                }
-                return { message: 'no winner yet', gameData: generateGameData(currentGame), nextMove: currentGame.nextMoveID };
+                message = 'no winner yet';
+                nextMove = currentGame.nextMoveID;
+                currentGame = generateGameData(currentGame);
             }
-            currentGame = await addMove(gameID, {
-                move: {
-                    roomID,
-                    type: 'cross',
-                    rowID,
-                    columnID,
-                    createdAt: new Date()
-                },
-                nextMove: 'circle',
-                nextMoveID: currentGame.roomIDs.joinee
-            });
-            return { message: 'no winner yet', gameData: generateGameData(currentGame), nextMove: currentGame.nextMoveID };
-        } return { message: 'cheater', gameData: generateGameData({ move: [] }), nextMove: currentGame.nextMoveID }
+        } else {
+            message = 'cheater';
+            nextMove = currentGame.nextMoveID;
+            currentGame = generateGameData({ move: [] });
+        }
+        return { message, gameData: currentGame, nextMove, winnerID }
     } catch (error) {
         console.error('error', error);
         return { message: 'error', gameData: generateGameData({ move: [] }), nextMove: currentGame.nextMoveID }
