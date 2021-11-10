6583b50f569628584f89e33240322e54950975dc
Piyush <piyush2396@gmail.com>
2021-11-10 19:37:57 +0530

    made changes for multiple sessions in a single room

--- models/games.cjs
+++ models/games.cjs
@@ -2,19 +2,24 @@ const mongoose = require('mongoose')
 
 const gamesSchema = new mongoose.Schema({
     id: { type: String, required: true, index: true },
-    roomIDs: {
+    playerIDs: {
         creator: { type: String, required: true },
         joinee: { type: String, required: false }
     },
     name: { type: String, required: true },
     nextMove: { type: String, defaultData: 'cross', required: true },
     nextMoveID: { type: String, defaultData: '', required: false },
-    moves: [{
-        roomID: { type: String, required: true },
-        type: { type: String, required: true },
-        rowID: { type: Number, required: true },
-        columnID: { type: Number, required: true },
-        createdAt: { type: Date, required: true },
+    games: [{
+        sessionID: { type: String, required: true, index: false },
+        winnerID: { type: String, defaultData: '', required: false },
+        finished: { type: Boolean, defaultData: false, required: false },
+        moves: [{
+            playerID: { type: String, required: true },
+            type: { type: String, required: true },
+            rowID: { type: Number, required: true },
+            columnID: { type: Number, required: true },
+            createdAt: { type: Date, required: true },
+        }],
     }],
     createdAt: { type: Date, required: true },
     updatedAt: { type: Date, required: true },
